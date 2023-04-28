cdf5d09d67bd5009b17ac49a4e520b4c74525f5a
pallyh <pallyx11@gmail.com>
2023-04-28 17:56:19 +0300

    Initial commit

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    private Rigidbody2D rb;  // ссылка на компонент
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();  // находим и получаем ссылку на "соседний" компонент объекта
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)
+            || Input.GetKeyDown(KeyCode.W)
+            || Input.GetKeyDown(KeyCode.UpArrow)
+            ) 
+        {
+            rb.AddForce(Vector2.up * 300f);
+        }
+    }
+}
